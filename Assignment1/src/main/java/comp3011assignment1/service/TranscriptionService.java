531c94187d3de8e5e07829f57c7a3efff7f150ee
cstubbin1 <100103359+cstubbin1@users.noreply.github.com>
2026-09-15 12:06:11 +0930

    I've just deleted the OpenAiConfig bean because apparently I didnt fully understand what I was doing and that it was making the key mandatory to run

--- Assignment1/src/main/java/comp3011assignment1/service/TranscriptionService.java
+++ Assignment1/src/main/java/comp3011assignment1/service/TranscriptionService.java
@@ -5,6 +5,7 @@ import java.io.ByteArrayInputStream;
 import org.springframework.stereotype.Service;
 
 import com.openai.client.OpenAIClient;
+import com.openai.client.okhttp.OpenAIOkHttpClient;
 import com.openai.models.audio.AudioModel;
 import com.openai.models.audio.transcriptions.TranscriptionCreateParams;
 import com.openai.models.audio.transcriptions.TranscriptionCreateResponse;
@@ -13,14 +14,10 @@ import comp3011assignment1.dto.TranscriptionResponse;
 
 @Service
 public class TranscriptionService {
-	
-	private final OpenAIClient client;
-	
-	public TranscriptionService (OpenAIClient client) {
-		this.client = client;
-	}
-	
+
 	public TranscriptionResponse transcribe(byte[] audioData) {
+		
+		OpenAIClient client = OpenAIOkHttpClient.fromEnv();
 		
 		TranscriptionCreateParams params =
 				TranscriptionCreateParams.builder()
